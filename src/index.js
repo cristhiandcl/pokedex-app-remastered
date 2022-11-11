0c0d24c9119019087d702de53dce85c49d499e3f
Cristhian David Carrillo Lopez <cristhiandcl8@MacBook-Pro-de-Cristhian.local>
2022-11-11 13:49:03 -0500

    fixed bug

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <Router>
+  <Router basename="/pokedex-app-remastered">
     <App />
   </Router>
   // </React.StrictMode>
