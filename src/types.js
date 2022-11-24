c3f271a9b8a93fc63001b61baa720e1935c78e22
Cristhian David Carrillo Lopez <carrillocristhian2408@gmail.com>
2022-11-24 11:51:05 -0500

    updates

---
+++ src/types.js
@@ -0,0 +1,110 @@
+export const types = {
+  normal: {
+    name: "normal",
+    color: "bg-stone-500",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/aa/Pok%C3%A9mon_Normal_Type_Icon.svg",
+  },
+  fighting: {
+    name: "fighting",
+    color: "bg-red-800",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/b/be/Pok%C3%A9mon_Fighting_Type_Icon.svg",
+  },
+  flying: {
+    name: "flying",
+    color: "bg-blue-200",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e0/Pok%C3%A9mon_Flying_Type_Icon.svg",
+  },
+  poison: {
+    name: "poison",
+    color: "bg-fuchsia-500",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c4/Pok%C3%A9mon_Poison_Type_Icon.svg",
+  },
+  ground: {
+    name: "ground",
+    color: "bg-yellow-100",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/8/8d/Pok%C3%A9mon_Ground_Type_Icon.svg",
+  },
+  rock: {
+    name: "rock",
+    color: "bg-yellow-600",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/b/bb/Pok%C3%A9mon_Rock_Type_Icon.svg",
+  },
+  bug: {
+    name: "bug",
+    color: "bg-lime-500",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/3/3c/Pok%C3%A9mon_Bug_Type_Icon.svg",
+  },
+  ghost: {
+    name: "ghost",
+    color: "bg-violet-600",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a0/Pok%C3%A9mon_Ghost_Type_Icon.svg",
+  },
+  steel: {
+    name: "steel",
+    color: "bg-slate-400",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/3/38/Pok%C3%A9mon_Steel_Type_Icon.svg",
+  },
+  fire: {
+    name: "fire",
+    color: "bg-red-500",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/5/56/Pok%C3%A9mon_Fire_Type_Icon.svg",
+  },
+  water: {
+    name: "water",
+    color: "bg-sky-500",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg",
+  },
+  grass: {
+    name: "grass",
+    color: "bg-green-500",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/f/f6/Pok%C3%A9mon_Grass_Type_Icon.svg",
+  },
+  electric: {
+    name: "electric",
+    color: "bg-yellow-500",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a9/Pok%C3%A9mon_Electric_Type_Icon.svg",
+  },
+  psychic: {
+    name: "psychic",
+    color: "bg-pink-300",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/ab/Pok%C3%A9mon_Psychic_Type_Icon.svg",
+  },
+  ice: {
+    name: "ice",
+    color: "bg-sky-400",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/8/88/Pok%C3%A9mon_Ice_Type_Icon.svg",
+  },
+  dragon: {
+    name: "dragon",
+    color: "bg-purple-600",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a6/Pok%C3%A9mon_Dragon_Type_Icon.svg",
+  },
+  dark: {
+    name: "dark",
+    color: "bg-amber-900",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/09/Pok%C3%A9mon_Dark_Type_Icon.svg",
+  },
+  fairy: {
+    name: "fairy",
+    color: "bg-pink-200",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/08/Pok%C3%A9mon_Fairy_Type_Icon.svg",
+  },
+};
